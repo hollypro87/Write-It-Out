9c168051b0277bd9a542a9da117177f413e0947c
jakevs <jakevs3@gmail.com>
2020-06-17 21:40:37 -0500

    added more to routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -8,7 +8,7 @@ module.exports = function (app) {
 
     // index route loads view.html
     app.get("/", function (req, res) {
-        res.sendFile(path.join(__dirname, "/index.html"));
+        res.resrender(path.join(__dirname, "index"));
     });
 
 };
