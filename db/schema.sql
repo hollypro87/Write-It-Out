152f843457bb315098b3de51cf743286f6553a1e
Nipa Dedhia <nipa.dedhia@gmail.com>
2020-06-22 14:23:23 -0500

    corrected db table name in htmlroute - inserted table in schema db

--- db/schema.sql
+++ db/schema.sql
@@ -2,3 +2,14 @@
 DROP DATABASE IF EXISTS write_it_out;
 -- Creates the "write it out" database --
 CREATE DATABASE write_it_out;
+USE write_it_out;
+CREATE TABLE Posts (
+id INTEGER NOT NULL auto_increment ,
+title VARCHAR(255) NOT NULL, 
+body TEXT NOT NULL,
+category VARCHAR(255) NOT NULL,
+createdAt DATETIME NOT NULL,
+updatedAt DATETIME NOT NULL, 
+PRIMARY KEY (id)
+);
+
