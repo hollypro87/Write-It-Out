908da09e0130a69533f072635530d85317e927b3
Nipa Dedhia <nipa.dedhia@gmail.com>
2020-06-22 21:29:49 -0500

    save and submit entries in app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,17 @@
 //event listeners, click handlers
+// Save Journal Entry
+$(document).on("click", "#submitButton", function () {
+  var id = $(this).data().id;
+  var comment = $("#journalEntryBody").val().trim();
+  $.post("/api/post/" + id, { body: comment });
+});
+
+// Submits a new post and brings user to blog page upon completion
+function submitPost(post) {
+  $.post("/api/posts", post, function () {
+    window.location.href = "/entry-form";
+  });
+}
 
 // Search Bar
 $(document).on("click", "#searchButton", function () {
