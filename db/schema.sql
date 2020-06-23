f370bbe2801f691721aa00cff16e03a48c529983
Nipa Dedhia <nipa.dedhia@gmail.com>
2020-06-22 20:28:29 -0500

    removed tables from schema file

--- db/schema.sql
+++ db/schema.sql
@@ -2,14 +2,5 @@
 DROP DATABASE IF EXISTS write_it_out;
 -- Creates the "write it out" database --
 CREATE DATABASE write_it_out;
-USE write_it_out;
-CREATE TABLE Posts (
-id INTEGER NOT NULL auto_increment ,
-title VARCHAR(255) NOT NULL, 
-body TEXT NOT NULL,
-category VARCHAR(255) NOT NULL,
-createdAt DATETIME NOT NULL,
-updatedAt DATETIME NOT NULL, 
-PRIMARY KEY (id)
-);
+
 
